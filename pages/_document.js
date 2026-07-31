0002bedd56b502941932c8ab59c7d726b3800142
Praveenkannan <praveenkicha01@gmail.com>
2026-07-31 09:41:22 +0530

    feat: VANGUARD animated hero landing page with fullscreen video, staggered animations, responsive mobile menu

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,18 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Existing fonts for other pages */}
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=DM+Sans:wght@200;300;400;500&family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* VANGUARD hero fonts */}
+        <link
+          href="https://db.onlinewebfonts.com/c/8b75d9dcff6a48c35a46656192adf019?family=FSP+DEMO+-+PODIUM+Sharp+4.11"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" defer />
